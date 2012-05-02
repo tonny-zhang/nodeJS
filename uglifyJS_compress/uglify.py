9e6ec2a7942580d2b17326a19d31ba9002599928
zk <wodexintiao@gmail.com>
2012-05-02 17:31:27 +0800

    uglify.py

---
+++ uglifyJS_compress/uglify.py
@@ -0,0 +1,19 @@
+import sublime, sublime_plugin,os
+
+class uglifyJS(sublime_plugin.EventListener):
+	def on_post_save(self, view):
+		_file_name = _formatPath(view.file_name());
+		_path = os.path.dirname(_file_name);
+		_nodeCommand = _formatPath('node E:/nodejsSite/uglifyJS_compress/compress.js ');
+
+		#path must be composed by slash (/),not by (\)
+		_siteJsPath = _formatPath('E:/fdx_git/fandongxi/site/js-source');
+		if(_path.startswith(_siteJsPath)):
+			os.system(_nodeCommand+_file_name);
+
+		file_object = open('uglifyInfo.txt', 'w')
+		file_object.write(_nodeCommand+_file_name+' '+_path+' '+_siteJsPath)
+		file_object.close();
+
+def _formatPath (uri):
+	return uri.replace('\\','/');
