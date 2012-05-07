316541f0f79a45883a0c6d80f78ac015e5b2e8ea
zk <wodexintiao@gmail.com>
2012-05-07 10:37:30 +0800

    set path of uglify.txt to base path of sublime

--- uglifyJS_compress/uglify.py
+++ uglifyJS_compress/uglify.py
@@ -8,14 +8,18 @@ class uglifyJS(sublime_plugin.EventListener):
 		#compress.js config
 		_nodeCommand = _formatPath('node E:/nodejsSite/uglifyJS_compress/compress.js ');
 
+		_compressInfo = 'no compress';
 		#path must be composed by slash (/),not by (\)
 		#site config
 		_siteJsPath = _formatPath('E:/fdx_git/fandongxi/site/js-source');
 		if(_path.startswith(_siteJsPath)):
+			_compressInfo = 'compress with uglify';
 			os.popen(_nodeCommand+_file_name);
 
-		file_info = open('uglifyInfo.txt', 'w')
+		#uglifyInfo.txt at base path of Sublime Text
+		file_info = open(sublime.packages_path()+'/../../uglifyInfo.txt', 'w')
 		file_info.write('nodeCommand: '+_nodeCommand+_file_name+'\n');
+		file_info.write(_compressInfo+'\n');
 		file_info.write('currentPath: '+_path+'\n');
 		file_info.write('siteJsPath: '+_siteJsPath);
 		file_info.close();
