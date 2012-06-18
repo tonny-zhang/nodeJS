4d87f4bcf914901d6785edfec4b2dd3db071f1f0
zk <wodexintiao@gmail.com>
2012-06-18 16:18:06 +0800

    add sitePath list

--- uglifyJS_compress/uglify.py
+++ uglifyJS_compress/uglify.py
@@ -1,27 +1,37 @@
-import sublime, sublime_plugin,os
+import sublime, sublime_plugin,os,json
 
 class uglifyJS(sublime_plugin.EventListener):
 	def on_post_save(self, view):
 		_file_name = _formatPath(view.file_name());
-		_path = os.path.dirname(_file_name);
+		_path = os.path.dirname(_file_name) + '/';
 
 		#compress.js config
 		_nodeCommand = _formatPath('node E:/nodejsSite/uglifyJS_compress/compress.js ');
 
 		_compressInfo = 'no compress';
 		#path must be composed by slash (/),not by (\)
-		#site config
-		_siteJsPath = _formatPath('E:/fdx_git/fandongxi/site/js-source');
-		if(_path.startswith(_siteJsPath)):
-			_compressInfo = 'compress with uglify';
-			os.popen(_nodeCommand+_file_name);
+		#site config,end width /
+		#ex:['site1/',['siteOld','siteNew']]
+		_siteJsPath = [_formatPath('E:/fdx_git/fandongxi/site/js-source/'),
+						[_formatPath('D:/GC/test/'),_formatPath('D:/GC/test-min/')]
+					  ];
+		for _jsPathConfig in _siteJsPath:
+			_checkPath = _jsPathConfig[0] if type(_jsPathConfig) == type([]) else _jsPathConfig;
+
+			if(_path.startswith(_checkPath)):
+				_compressInfo = 'compress with uglify';
+				_nodeCommand += _file_name+' ';
+				_nodeCommand += _file_name.replace(_jsPathConfig[0],_jsPathConfig[1]) if (type(_jsPathConfig) == type([]) and len(_jsPathConfig) > 1) else '';
+				os.popen(_nodeCommand);
+				break;
 
 		#uglifyInfo.txt at base path of Sublime Text
 		file_info = open(sublime.packages_path()+'/../../uglifyInfo.txt', 'w')
-		file_info.write('nodeCommand: '+_nodeCommand+_file_name+'\n');
+		file_info.write('nodeCommand: '+_nodeCommand+'\n');
 		file_info.write(_compressInfo+'\n');
 		file_info.write('currentPath: '+_path+'\n');
-		file_info.write('siteJsPath: '+_siteJsPath);
+		file_info.write('checkPath: '+_checkPath+'\n');
+		file_info.write('siteJsPath: '+json.dumps(_siteJsPath)+'\n');
 		file_info.close();
 
 def _formatPath (uri):
